c59b91fd9e82b69dfea7b9c01771a5a0b2b049bd
vviimm <wowaonrails@gmail.com>
2016-05-27 17:13:25 +0300

    Changed application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Miw
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.assets.initialize_on_precompile = false
+
     config.to_prepare do
       Devise::SessionsController.layout "cezar"
     end
