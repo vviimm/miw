2904467fc658f82fbc508915f73773c215d5a431
vviimm <wowaonrails@gmail.com>
2016-05-27 18:44:34 +0300

    Changed task settingsPage

--- app/tasks/page_settings.rb
+++ app/tasks/page_settings.rb
@@ -9,14 +9,16 @@ class PageSettings
 
     def add_home_settings!
 
+      page_id = 1
       title = "Мой кабинет"
       subtitle = "любимые страницы"
       banner = File.open("#{Rails.root}/db/images/home.jpg")
 
-      existing_page_settings = SettingsPage.find_by(name: name)
+      existing_page_settings = SettingsPage.find_by(page_id: page_id)
 
       if existing_page_settings.blank?
         SettingsPage.create!(
+          page_id:  page_id,
           title:    title,
           subtitle: subtitle,
           banner:   banner
@@ -26,6 +28,7 @@ class PageSettings
 
     def add_about_settings!
 
+      page_id = 2
       title = "Немного обо мне"
       subtitle = "молодость, юность, отрочество"
       banner = File.open("#{Rails.root}/db/images/about.jpg")
@@ -34,6 +37,7 @@ class PageSettings
 
       if existing_page_settings.blank?
         SettingsPage.create!(
+          page_id:  page_id,
           title:    title,
           subtitle: subtitle,
           banner:   banner
@@ -43,6 +47,7 @@ class PageSettings
 
     def add_contact_settings!
 
+      page_id = 3
       title = "Контакты"
       subtitle = "что, где, когда"
       banner = File.open("#{Rails.root}/db/images/contact.jpg")
@@ -51,6 +56,7 @@ class PageSettings
 
       if existing_page_settings.blank?
         SettingsPage.create!(
+          page_id:  page_id,
           title:    title,
           subtitle: subtitle,
           banner:   banner
